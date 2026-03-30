ce89e5d7b4aad6bc515eae40c631804d3713ee31
11251045-glitch <11251045@st.fhjh.tp.edu.tw>
2026-03-30 16:00:33 +0800

    Add service worker for U-Sport caching and notifications

---
+++ sw.js
@@ -0,0 +1,24 @@
+// U-Sport 集點小幫手 · Service Worker 2026
+const CACHE = 'usport-v1';
+const ASSETS = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks =>
+    Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
+
+self.addEventListener('notificationclick', e => {
+  e.notification.close();
+  e.waitUntil(clients.openWindow('https://usport.gov.taipei'));
+});
